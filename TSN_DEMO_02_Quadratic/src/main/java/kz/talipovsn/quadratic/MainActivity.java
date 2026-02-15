a70c533465729ba95187dd3698d17db2620c0c29
DDoubleMaster <130893087+DDoubleMaster@users.noreply.github.com>
2026-02-15 17:38:11 +0500

    Lesson 2 was completed

--- TSN_DEMO_02_Quadratic/src/main/java/kz/talipovsn/quadratic/MainActivity.java
+++ TSN_DEMO_02_Quadratic/src/main/java/kz/talipovsn/quadratic/MainActivity.java
@@ -11,8 +11,8 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     // Локальные переменные для доступа к компонентам окна
-    private EditText editText_a, editText_b, editText_c;
-    private TextView textView_x1, textView_x2;
+    private EditText editText_x, editText_a, editText_b;
+    private TextView textView_y;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,25 +20,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Инициализация переменных доступа к компонентам окна
+        editText_x = findViewById(R.id.editText_x);
         editText_a = findViewById(R.id.editText_a);
         editText_b = findViewById(R.id.editText_b);
-        editText_c = findViewById(R.id.editText_c);
-        textView_x1 = findViewById(R.id.textView_x1);
-        textView_x2 = findViewById(R.id.textView_x2);
+        textView_y = findViewById(R.id.textView_y);
 
         // Проверка на переворот экрана и восстановление нужных значений в компонентах
         if (savedInstanceState != null) {
-            textView_x1.setText(savedInstanceState.getString("x1"));
-            textView_x2.setText(savedInstanceState.getString("x2"));
+            textView_y.setText(savedInstanceState.getString("y"));
         }
     }
 
+    @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
 
         // Сохранение нужных нам значений компонент при перевороте экрана
-        outState.putString("x1", textView_x1.getText().toString());
-        outState.putString("x2", textView_x2.getText().toString());
+        outState.putString("y", textView_y.getText().toString());
     }
 
     // МЕТОД ДЛЯ КНОПКИ РАСЧЕТА
@@ -46,13 +44,13 @@ public class MainActivity extends AppCompatActivity {
     public void onCalc(View v) {
 
         // Локальные переменные
-        double a, b, c, d, x1, x2;
+        double x, a, b, y;
 
         try {
             // Считывание введенных входных значений в переменные
+            x = Double.parseDouble(editText_x.getText().toString());
             a = Double.parseDouble(editText_a.getText().toString());
             b = Double.parseDouble(editText_b.getText().toString());
-            c = Double.parseDouble(editText_c.getText().toString());
         } catch (Exception e) {
             // Выдача всплывающего сообщения на экран об ошибке
             Toast.makeText(MainActivity.this, "Неверные входные данные!",
@@ -60,21 +58,23 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
 
-        d = (b * b) - 4 * a * c; // Расчет дискриминанта
-
         // Расчет значений x1 и x2
         try {
-            x1 = (-b + Math.sqrt(d)) / (2 * a);
-            x2 = (-b - Math.sqrt(d)) / (2 * a);
-            if (Double.isNaN(x1) || Double.isNaN(x2) || Double.isInfinite(x1) || Double.isInfinite(x2)) {
-                throw new Exception();
+            if (x >= 4) {
+                if (a + b == 0){
+                    textView_y.setText("Сумма A и B не должно быть 0!");
+                    return;
+                }
+                y = 10 * (x + Math.pow(a, 2));
+                y /= b + a;
+            } else{
+                y = 5 * (x + Math.pow(a, 2) + b);
             }
-            textView_x1.setText(String.format("x1 = %.3f", x1));
-            textView_x2.setText(String.format("x2 = %.3f", x2));
+
+            textView_y.setText(String.valueOf(y));
         } catch (Exception e) {
             String err = "Нет решения!";
-            textView_x1.setText(err);
-            textView_x2.setText(err);
+            textView_y.setText(err);
         }
 
     }
